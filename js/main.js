d91fefcc900ce11feae20b862909d46866de4edc
Nadtochii <alexandra.nadtochiy.v@gmail.com>
2016-10-14 14:21:56 +0300

    Add moving block

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 var ROWS=20, COLS=20, BLOCK_WIDTH=5, BLOCK_HEIGHT=1;
 //IDs
 var EMPTY=0, BLOCK=1, BUILDING=2;
+//Directions
+var LEFT=0, RIGHT=1, DOWN=2;
 //Key codes
 var KEY_SPASE=13;
 
@@ -32,7 +34,28 @@ var grid = {
 	}
 }
 
-var canvas;
+var block = {
+	direction: null,
+	last: null,
+	_queue: null,
+
+	init: function(direction, x, y) {
+		this.direction = direction;
+		this._queue = [];
+		this.insert(x, y);
+	},
+
+	insert: function(x, y) {
+		this._queue.unshift({x:x, y:y});
+        this.last = this._queue[0];
+	},
+
+	remove: function() {
+		return this._queue.pop();
+	}
+}
+
+var canvas, frames, keystate;
 function main() {
 	canvas = document.createElement("canvas");
 	canvas.width =COLS*20;
@@ -40,15 +63,81 @@ function main() {
 	ctx = canvas.getContext("2d");
 
 	var container = document.getElementById("container");
+	frames = 0;
+	keystate = {};
+
+	document.addEventListener("keydown", function(event) {
+		keystate[event.keyCode] = true;
+	});
+	document.addEventListener("keyup", function(event) {
+		delete keystate[event.keyCode];
+	});
 
     container.appendChild(canvas);
 
     init();
-    draw();
+    loop();
 }
 
 function init() {
 	grid.init(EMPTY, COLS, ROWS);
+
+	var bp = {x:COLS-1, y:3};
+	block.init(LEFT, bp.x, bp.y);
+	grid.set(BLOCK, bp.x, bp.y);
+}
+
+function loop() {
+	update();
+	draw();
+
+	window.requestAnimationFrame(loop, canvas);
+}
+
+function update() {
+	frames++;
+
+	if ( keystate[KEY_SPASE] ) {
+		block.direction = DOWN;
+	}
+
+	if (frames%7 === 0) {
+		var bx = block.last.x;
+		var by = block.last.y;
+
+		switch(block.direction) {
+			case LEFT:
+				bx--;
+				break;
+			case RIGHT:
+				bx++;
+				break;
+			case DOWN:
+				by++;
+				break;
+		}
+
+		if ( bx < 1 ) {
+			block.direction = RIGHT;
+		}
+		if ( bx > COLS-2 ) {
+			block.direction = LEFT;
+		}
+
+		if ( by > ROWS-2 ) {
+
+		}
+
+		var out = block.remove();
+		grid.set(EMPTY, out.x, out.y);
+
+		out.x = bx;
+		out.y = by;
+
+		grid.set(BLOCK, out.x, out.y);
+		block.insert(out.x, out.y);
+
+	}
 }
 
 function draw() {
@@ -68,7 +157,7 @@ function draw() {
 					ctx.fillStyle = "#f00";
 					break;
 			}
-			ctx.fillRect(i*tw, i*th, tw, th);
+			ctx.fillRect(i*tw, j*th, tw, th);
 		}
 	}
 }
